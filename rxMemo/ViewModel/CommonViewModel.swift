bbb55cdb3d4a54c8112e6c1de42f3f870943f08c
Moon HyoJae <followmoonrise@gmail.com>
2020-09-15 12:56:47 +0900

    Implement MemoListViewModel

---
+++ rxMemo/ViewModel/CommonViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  CommonViewModel.swift
+//  rxMemo
+//
+//  Created by 문효재 on 2020/09/15.
+//  Copyright © 2020 Moon HyoJae. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CommonViewModel: NSObject {
+    let title : Driver<String>
+    let sceneCoordinator: SceneCoordinatorType
+    let storage:MemoStorageType
+    
+    init(title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.title = Observable.just(title).asDriver(onErrorJustReturn: "")
+        self.sceneCoordinator = sceneCoordinator
+        self.storage = storage
+    }
+}
