75e51c2516067a4133bb8ec9537790952c6b8657
Moon HyoJae <followmoonrise@gmail.com>
2020-09-15 10:26:40 +0900

    Add Memo Storage

---
+++ rxMemo/Service/MemoStorageType.swift
@@ -0,0 +1,25 @@
+//
+//  MemoStorageType.swift
+//  rxMemo
+//
+//  Created by 문효재 on 2020/09/15.
+//  Copyright © 2020 Moon HyoJae. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content:String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
