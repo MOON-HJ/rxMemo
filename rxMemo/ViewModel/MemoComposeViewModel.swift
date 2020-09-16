27d2da95a8f98bc1361e3b8eeaf28cd55f979e71
Moon HyoJae <followmoonrise@gmail.com>
2020-09-16 17:07:45 +0900

    Implement MemoComposeViewModel

--- rxMemo/ViewModel/MemoComposeViewModel.swift
+++ rxMemo/ViewModel/MemoComposeViewModel.swift
@@ -7,6 +7,40 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoComposeViewModel{
+class MemoComposeViewModel: CommonViewModel{
+    private let content: String?
+    var inititalText: Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+        
+    }
+    
+    let saveAction: Action<String,Void>
+    let cancleAction: CocoaAction
+    
+    init(title: String, content: String? = nil, sceneCoordinator: SceneCoordinator, storage: MemoStorageType, saveAction: Action<String, Void>? = nil, cancleAction: CocoaAction? = nil) {
+        self.content = content
+        
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        self.cancleAction = CocoaAction {
+            if let action = cancleAction {
+                action.execute()
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map{ _ in }
+        }
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+        
+    }
 }
