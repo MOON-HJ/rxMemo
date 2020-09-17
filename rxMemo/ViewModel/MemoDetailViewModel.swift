193da1b63fa5f70207fab5d7bf29f211603e3319
Moon HyoJae <followmoonrise@gmail.com>
2020-09-17 11:47:28 +0900

    Implements MemoDetailViewModel

--- rxMemo/ViewModel/MemoDetailViewModel.swift
+++ rxMemo/ViewModel/MemoDetailViewModel.swift
@@ -7,7 +7,30 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel{
+class MemoDetailViewModel: CommonViewModel{
+    let memo: Memo
     
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    var contents: BehaviorSubject<[String]>
+    
+    init(memo : Memo, title:String, sceneCooordinator: SceneCoordinator, storage: MemoStorageType) {
+        self.memo = memo
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content, formatter.string(from: memo.insertDate)])
+        
+        super.init(title: title, sceneCoordinator: sceneCooordinator, storage: storage)
+        
+        
+    }
 }
