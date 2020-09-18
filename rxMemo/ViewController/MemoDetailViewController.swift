369b8fdad8f3313045bf380bfc07dff36f99df28
Moon HyoJae <followmoonrise@gmail.com>
2020-09-18 11:15:46 +0900

    Memo Delete

--- rxMemo/ViewController/MemoDetailViewController.swift
+++ rxMemo/ViewController/MemoDetailViewController.swift
@@ -61,5 +61,6 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
                 self?.present(vc, animated: true, completion: nil)
             }).disposed(by: rx.disposeBag)
         
+        deleteButton.rx.action = viewModel.makeDeleteAction()
     }
 }
